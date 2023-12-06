33659bc4cac2674e7890b7cfcc45dd94b1e0c920
tiennam <phamtiennam2301@gmail.com>
2023-12-06 11:15:49 +0700

    feat: remove the snapshot statedb

--- core/types/simulated_tx.go
+++ core/types/simulated_tx.go
@@ -2,7 +2,6 @@ package types
 
 import (
 	"encoding/json"
-	"math/big"
 )
 
 type SimulationDebugInfoResponse struct {
@@ -12,8 +11,6 @@ type SimulationDebugInfoResponse struct {
 
 type SimulationTxResponse struct {
 	PendingBlockNumber uint64                      `json:"pending_block_number"`
-	BaseFee            *big.Int                    `json:"base_fee"`
-	CurrentBlockTime   uint64                      `json:"current_block_time"`
 	DebugInfo          SimulationDebugInfoResponse `json:"debug_info"`
 	CallFrame          json.RawMessage             `json:"call_frame"`
 }
