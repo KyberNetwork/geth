cf7ec60d8020aaef0275f98cbef125108898f823
(Nam) Pham Tien <phamtiennam2301@gmail.com>
2024-03-11 14:45:06 +0700

    feat: estimate gas for bundles

--- eth/backend.go
+++ eth/backend.go
@@ -366,7 +366,9 @@ func (s *Ethereum) Synced() bool                       { return s.handler.synced
 func (s *Ethereum) SetSynced()                         { s.handler.enableSyncedFeatures() }
 func (s *Ethereum) ArchiveMode() bool                  { return s.config.NoPruning }
 func (s *Ethereum) BloomIndexer() *core.ChainIndexer   { return s.bloomIndexer }
-
+func (s *Ethereum) Config() *ethconfig.Config {
+	return s.config
+}
 // Protocols returns all the currently configured
 // network protocols to start.
 func (s *Ethereum) Protocols() []p2p.Protocol {
